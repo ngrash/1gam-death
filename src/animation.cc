39f9ef552b84fbe527944e150f364d410c5d3d09
Nico Grashoff <ngrash@gmail.com>
2018-05-18 22:11:41 +0200

    Only animate when there is morgen than one frame

--- src/animation.cc
+++ src/animation.cc
@@ -3,6 +3,7 @@
 Animation::Animation()
 {
   render_flip = SDL_FLIP_NONE;
+  num_frames = 0;
   src_rect.x = 0;
   src_rect.y = 0;
   src_rect.w = 16;
@@ -16,15 +17,17 @@ void Animation::Reset() {
 }
 
 void Animation::Update(float seconds_elapsed) {
-  current_frame_duration_ += seconds_elapsed;
-  if(current_frame_duration_ >= frame_duration) {
+  if(num_frames > 1) {
+    current_frame_duration_ += seconds_elapsed;
+    if(current_frame_duration_ >= frame_duration) {
 
-    current_frame_++;
-    if(current_frame_ >= num_frames) {
-      current_frame_ = 0;
+      current_frame_++;
+      if(current_frame_ >= num_frames) {
+        current_frame_ = 0;
+      }
+
+      this->src_rect.x = current_frame_ * 16;
+      current_frame_duration_ = 0;
     }
-
-    this->src_rect.x = current_frame_ * 16;
-    current_frame_duration_ = 0;
   }
 }
